5c8677c09c746ab0ed5dd9f151288e05347249b0
Eron Cohen <eroncohen10@gmail.com>
2020-02-03 16:34:08 +0200

    dlib preprocess

---
+++ create_data_file_dlib.py
@@ -0,0 +1,32 @@
+import os
+import csv
+from preprocessing_dlib import get_landmarks
+
+
+def create_csv_file():
+    csv_file = open('landsmark.csv', 'w', newline='')
+    obj = csv.writer(csv_file)
+    arr = []
+    arr.append('Emotion')
+    for i in range(37, 69):
+        arr.append(str(i) + "d")
+        arr.append(str(i) + "a")
+    obj.writerow(arr)
+    directory_name_list = ['Training', 'PublicTest', 'PrivateTest']
+    for name in directory_name_list:
+        for x in range(7):
+            if x == 3:
+                emotion = 1
+            else:
+                emotion = 0
+            directory_name = 'C:/Users/Eron/PycharmProjects/Final_Project/' + name + '/' + str(x)
+            directory = os.fsencode(directory_name)
+
+            for file in os.listdir(directory):
+                filename = os.fsdecode(file)
+                arr_to_write = get_landmarks(str(directory_name) + '/' + str(filename))
+                if arr_to_write != 0:
+                    print(str(directory_name) + '/' + str(filename))
+                    arr_to_write.insert(0, emotion)
+                    obj.writerow(arr_to_write)
+    csv_file.close()
---
+++ preprocessing_dlib.py
@@ -0,0 +1,44 @@
+import cv2
+import dlib
+import numpy as np
+import math
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+data = {}
+
+
+def get_landmarks(image_path):
+    eyes_and_mouth_points = []
+    image = cv2.imread(image_path)
+    detections = detector(image, 1)
+    for k, d in enumerate(detections): #For all detected face instances individually
+        shape = predictor(image, d) #Draw Facial Landmarks with the predictor class
+        xlist = []
+        ylist = []
+        for i in range(0, 68): #Store X and Y coordinates in two lists
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+        xmean = np.mean(xlist) #Find both coordinates of centre of gravity
+        ymean = np.mean(ylist)
+        xcentral = [(x-xmean) for x in xlist] #Calculate distance centre <-> other points in both axes
+        ycentral = [(y-ymean) for y in ylist]
+        landmarks_vectorised = []
+        i = 1
+        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+            #landmarks_vectorised.append(w)
+            landmarks_vectorised.append(i)
+            meannp = np.asarray((ymean, xmean))
+            coornp = np.asarray((z, w))
+            dist = np.linalg.norm(coornp-meannp)
+            if i > 36:
+                eyes_and_mouth_points.append(dist)
+                eyes_and_mouth_points.append((math.atan2(y, x)*360)/(2*math.pi))
+            landmarks_vectorised.append(dist)
+            landmarks_vectorised.append((math.atan2(y, x)*360)/(2*math.pi))
+            i = i + 1
+        data['landmarks_vectorised'] = landmarks_vectorised
+    if len(detections) < 1:
+        data['landmarks_vectorised'] = "error"
+        return 0  # no face found
+    return eyes_and_mouth_points
